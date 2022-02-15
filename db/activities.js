ef109355ab01a83046aa1229a828a1066846cb39
Bryan Num <bnum33@gmail.com>
2022-02-15 12:04:27 -0800

    made all adapter functions for activities table

--- db/activities.js
+++ db/activities.js
@@ -1,16 +1,70 @@
-// getActivityById
-// getActivityById(id)
-// return the activity
+const { client } = require("./client");
 
-// getAllActivities
-// select and return an array of all activities
+async function getActivityById(id) {
+  try {
+    const { rows } = await client.query(`
+    SELECT id FROM activites
+    WHERE id = ${id};
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// createActivity
-// createActivity({ name, description })
-// return the new activity
+async function getAllActivites() {
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM activities
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// updateActivity
-// updateActivity({ id, name, description })
+async function createActivity({ name, description }) {
+  try {
+    const lwrName = name.toLowerCase();
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+    INSERT INTO activites (name, description)
+    VALUES($1,$2)
+    ON CONFLICT (name, description) DO NOTHING
+    RETURNING*;
+    `,
+      [lwrName, description]
+    );
+    return activity; // return the new activity
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateActivity({ id, name, description }) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(`
+    UPDATE activites
+    SET name = ${name}, description = ${description}
+    WHERE id = ${id};
+    RETURNING*
+    `);
+    return activity; // return the new activity
+  } catch (error) {
+    throw error;
+  }
+}
 // don't try to update the id
 // do update the name and description
 // return the updated activity
+
+module.exports = {
+  getActivityById,
+  getAllActivites,
+  // createActivity,
+  // updateActivity
+};
