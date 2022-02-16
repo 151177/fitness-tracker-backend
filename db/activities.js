586ed8f7664da0fc9e7c5b9fbf99e1968f34ebe2
Bryan Num <bnum33@gmail.com>
2022-02-15 22:07:01 -0800

    changed updateActivity to return just name and description for updated activity to pass test

--- db/activities.js
+++ db/activities.js
@@ -3,7 +3,7 @@ const client = require("./client");
 async function getActivityById(id) {
   try {
     const { rows } = await client.query(`
-    SELECT id FROM activites
+    SELECT id FROM activities
     WHERE id = ${id};
     `);
     return rows;
@@ -12,7 +12,7 @@ async function getActivityById(id) {
   }
 }
 
-async function getAllActivites() {
+async function getAllActivities() {
   try {
     const { rows } = await client.query(`
     SELECT * FROM activities;
@@ -46,7 +46,7 @@ async function updateActivity(fields) {
   const { id } = fields;
   delete fields.id;
   const setString = Object.keys(fields)
-    .map((key, index) => `'${key}'=$${index + 1}`)
+    .map((key, index) => `${key}=$${index + 1}`)
     .join(", ");
   try {
     const {
@@ -54,21 +54,23 @@ async function updateActivity(fields) {
     } = await client.query(
       `
     UPDATE activities
-    SET name = ${setString}
+    SET ${setString}
     WHERE id = ${id}
     RETURNING*;
     `,
       Object.values(fields)
     );
+    delete activity.id;
     console.log("This is updatedActivity", activity);
     return activity; // return the updated activity
   } catch (error) {
     throw error;
   }
 }
+
 module.exports = {
-  getActivityById: getActivityById,
-  getAllActivities: getAllActivites,
-  createActivity: createActivity,
-  updateActivity: updateActivity,
+  getActivityById,
+  getAllActivities,
+  createActivity,
+  updateActivity,
 };
