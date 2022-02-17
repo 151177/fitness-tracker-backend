e7cb2c702236f20c6b270b8da12a6815e51b976a
Bryan Num <bnum33@gmail.com>
2022-02-16 19:54:43 -0800

    added JWT verify method and handler to set a user when a user logs in in  api/index.js

--- api/index.js
+++ api/index.js
@@ -3,11 +3,48 @@
 // export the api router
 const express = require("express");
 const apiRouter = express.Router();
+const usersRouter = require("./users");
+const activitiesRouter = require("./activities");
 const {} = require("../db");
 require("dotenv").config();
 
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
+
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+apiRouter.use(async (req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+  next();
+});
 
 apiRouter.get("/health", (req, res, next) => {
   try {
@@ -21,10 +58,8 @@ apiRouter.get("/health", (req, res, next) => {
   }
 });
 
-const usersRouter = require("./users");
+// api routes
 apiRouter.use("/users", usersRouter);
-
-const activitiesRouter = require("./activities");
 apiRouter.use("/activities", activitiesRouter);
 
 module.exports = apiRouter;
