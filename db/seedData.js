0a2482190d9c57217842729e68d6b7143039690f
Bryan Num <bnum33@gmail.com>
2022-02-17 22:18:33 -0800

    added getRoutinesWithoutActivites and addActivityToRoutine functions

--- db/seedData.js
+++ db/seedData.js
@@ -1,5 +1,12 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-const { createUser, createActivity, createRoutine } = require("./");
+const {
+  createUser,
+  createActivity,
+  createRoutine,
+  getRoutinesWithoutActivities,
+  getAllActivities,
+  addActivityToRoutine,
+} = require("./");
 const client = require("./client");
 
 async function dropTables() {
@@ -244,7 +251,7 @@ async function rebuildDB() {
     await createInitialUsers();
     await createInitialActivities();
     await createInitialRoutines();
-    // await createInitialRoutineActivities();
+    await createInitialRoutineActivities();
   } catch (error) {
     console.log("Error during rebuildDB");
     throw error;
