a7bab865504c715423db3ae7a84736bd1fa7894a
Bryan Num <bnum33@gmail.com>
2022-02-21 20:45:36 -0800

    added POST route for creating a new routine

--- api/routines.js
+++ api/routines.js
@@ -19,6 +19,7 @@ routineRouter.use((req, res, next) => {
   next();
 });
 
+// GET /routines
 routineRouter.get("/", async (req, res, next) => {
   try {
     const routines = await getAllPublicRoutines();
@@ -27,6 +28,25 @@ routineRouter.get("/", async (req, res, next) => {
     next({
       name: "NoPublicRoutines",
       message: "Unable to get all public routines",
+    });
+  }
+});
+
+//POST /routines (*)
+routineRouter.post("/", requireUser, async (req, res, next) => {
+  try {
+    const routineFields = {
+      creatorId: req.user.id,
+      isPublic: req.body.isPublic,
+      name: req.body.name,
+      goal: req.body.goal,
+    };
+    const newRoutine = await createRoutine(routineFields);
+    res.send(newRoutine);
+  } catch (error) {
+    next({
+      name: "FailedToPostRoutine",
+      message: "This routine could not be posted",
     });
   }
 });
