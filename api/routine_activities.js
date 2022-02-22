663b9c76ac1b5093f0bfe7bc0a82a313479daa5c
Bryan Num <bnum33@gmail.com>
2022-02-21 17:45:20 -0800

    started working on routine-activities-routes

--- api/routine_activities.js
+++ api/routine_activities.js
@@ -1,30 +1,60 @@
 const express = require("express");
-const r_aRouter = express.Router();
+const routineActivitiesRouter = express.Router();
 const { requireUser } = require("./utils");
-const { updateRoutineActivity } = require("../db/index");
+const {
+  getRoutineActivityById,
+  updateRoutineActivity,
+} = require("../db/index");
 
-r_aRouter.use((req, res, next) => {
+routineActivitiesRouter.use((req, res, next) => {
   console.log("A request is made to /routine_activities");
   next();
 });
 
 // PATCH /routine_activities/:routineActivityId (**)
 // Update the count or duration on the routine activity
-//! NOT DONE
-r_aRouter.patch("/:routineActivityId", requireUser, async (req, res, next) => {
-  const id = req.params.routineActivityId;
-  const { count, duration } = req.body;
-
-  try {
-    const updates = await updateRoutineActivity(id, req.body);
-    console.log(updates);
-    res.send("THESE ARE MY UPDATES", updates);
-  } catch (error) {
-    next(error);
-  }
-});
+//! Uncomment once done with making POST /routines/:routineId/activities route
+// routineActivitiesRouter.patch(
+//   "/:routineActivityId",
+//   requireUser,
+//   async (req, res, next) => {
+//     try {
+//       const id = req.params.routineActivityId;
+//       const { count, duration } = req.body;
+//       const oldRoutineActivity = await getRoutineActivityById(id);
+//       const updates = {
+//         id: id,
+//         count: oldRoutineActivity.count,
+//         duration: oldRoutineActivity.duration,
+//       };
+//       if (!count) {
+//         updates.count = count;
+//       }
+//       if (!duration) {
+//         updates.duration = duration;
+//       }
+//       const updatedRoutineActivity = await updateRoutineActivity(updates);
+//       console.log("THESE ARE MY UPDATES", updatedRoutineActivity);
+//       res.send(updatedRoutineActivity);
+//     } catch ({ name, message }) {
+//       next({ name, message });
+//     }
+//   }
+// );
 
 // DELETE /routine_activities/:routineActivityId (**)
 // Remove an activity from a routine, use hard delete
+//! Uncomment later
+// routineActivitiesRouter.delete(
+//   "/:routineActivityId",
+//   requireUser,
+//   async (req, res, next) => {
+//     try {
+//       res.send("Delete in Progress");
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
-module.exports = r_aRouter;
+module.exports = routineActivitiesRouter;
