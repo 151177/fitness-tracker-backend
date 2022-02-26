88ace74e8a351d42e68b2c9e204ea8b4a63b92b4
Bryan Num <bnum33@gmail.com>
2022-02-26 09:45:21 -0800

    added checkOwner authorization function

--- api/utils.js
+++ api/utils.js
@@ -1,3 +1,5 @@
+const { getRoutineById } = require("../db");
+
 // checks to see if a user is logged in
 function requireUser(req, res, next) {
   if (!req.user) {
@@ -9,6 +11,17 @@ function requireUser(req, res, next) {
   next();
 }
 
+// function to check owner of routine
+async function checkOwner(userId, routineId) {
+  try {
+    const routine = await getRoutineById(routineId);
+    return routine.creatorId === userId;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   requireUser,
+  checkOwner,
 };
